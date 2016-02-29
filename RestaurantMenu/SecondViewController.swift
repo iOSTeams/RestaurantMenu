bb015832f6017f14c10c575dc969ce33726239a7
Justin Lee <jlee402@mail.ccsf.edu>
2016-02-29 14:02:50 -0800

    Sliders

--- RestaurantMenu/SecondViewController.swift
+++ RestaurantMenu/SecondViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var sliderPercentage: UILabel!
+    @IBOutlet weak var taxSlider: UISlider!
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var amountTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        var userInput = amountTextField.text
+        
+        print (userInput!)
 
+        
+    }
+    @IBAction func amountButtonPressed(sender: AnyObject) {
+           }
+
+
+    @IBAction func rateChangeSlider(sender: AnyObject) {
+        
+//        // Set the label text to the value of the slider as it changes
+//        self.label.text = [NSString stringWithFormat:@"%f", self.slider.value];
+        self.sliderPercentage.text = self.sliderPercentage.valueForKey
+    }
 
 }
 
